8da81483798c67a8de2dc23ad1e8781af479788c
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-06-14 21:30:19 +0900

    add ui type

---
+++ frontend/types/ui.ts
@@ -0,0 +1,16 @@
+export enum LoadingState {
+  IDLE = 'idle',
+  LOADING = 'loading',
+  SUCCESS = 'success',
+  ERROR = 'error'
+}
+
+export enum DialogType {
+  SAVE_PLAYLIST = 'save_playlist',
+  NONE = 'none'
+}
+
+export enum FileType {
+  M3U = 'm3u',
+  M3U8 = 'm3u8'
+}
