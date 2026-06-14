341730bfbd2aa42509ee548fa3bf089b013127c8
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-06-14 21:30:09 +0900

    add login f orm h eader component

---
+++ frontend/components/login/LoginFormHeader.tsx
@@ -0,0 +1,11 @@
+const LoginFormHeader = () => {
+  return (
+    <div className="flex flex-col items-center gap-2 text-center">
+      <h1 className="text-4xl font-bold">Start Watching</h1>
+      <p className="text-neutral-400 text-lg text-balance">Select a playlist below</p>
+    </div>
+  );
+};
+
+export default LoginFormHeader;
+
