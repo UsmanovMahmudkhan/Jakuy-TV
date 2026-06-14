04730e6632aa827a04822b937fef519f8ae1f80d
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-06-14 21:30:08 +0900

    add favorites e mpty s tate component

---
+++ frontend/components/browse/FavoritesEmptyState.tsx
@@ -0,0 +1,11 @@
+const FavoritesEmptyState = () => {
+  return (
+    <div className="text-center py-20">
+      <p className="text-neutral-500 text-lg">No favorites yet</p>
+      <p className="text-neutral-600 text-sm mt-2">Click the heart icon on any channel to add it here</p>
+    </div>
+  );
+};
+
+export default FavoritesEmptyState;
+
