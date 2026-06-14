c91d714fdcbeaacb8bc3e0772b7ced6bc107b88f
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-06-14 21:29:38 +0900

    add jakuy t v a pplication backend file

---
+++ backend/src/main/java/com/jakuy/tv/JakuyTvApplication.java
@@ -0,0 +1,12 @@
+package com.jakuy.tv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JakuyTvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JakuyTvApplication.class, args);
+    }
+}
