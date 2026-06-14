3b39b1b95a26bc5f65e6e0cd1ee9c13f7a9a5d93
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-06-14 21:29:39 +0900

    add countries backend file

---
+++ backend/src/main/java/com/jakuy/tv/common/Countries.java
@@ -0,0 +1,11 @@
+package com.jakuy.tv.common;
+
+public final class Countries {
+
+    public static final String UZBEKISTAN = "Uzbekistan";
+    public static final String UZ_CODE = "UZ";
+    public static final String UZBEK_LANGUAGE = "Uzbek";
+
+    private Countries() {
+    }
+}
